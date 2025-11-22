acda46909d28fb57e6cb451ca07d46d6ef9089e7
Arthur Fellipe <vpneuaif@gmail.com>
2025-11-22 19:26:04 -0300

    Add storage.h for player progress functions

---
+++ src/storage.h
@@ -0,0 +1,9 @@
+#ifndef RPG_EM_C_STORAGE_H
+#define RPG_EM_C_STORAGE_H
+
+#include "rpg.h"
+
+void salvar_progresso(struct jogador *player);
+int carregar_progresso(struct jogador *player);
+
+#endif //RPG_EM_C_STORAGE_H
