b29ec7cebf6cb7628ead88dabac898d4c62c15b1
Arthur Fellipe <vpneuaif@gmail.com>
2025-11-22 19:28:22 -0300

    Add subirLevel function declaration to combate.h

--- include/combate.h
+++ include/combate.h
@@ -9,5 +9,6 @@ int turno(struct jogador *player, struct inimigo *enemy, int boss);
 int gerarMonstro(struct jogador *player, struct inimigo *inimigo);
 int danoMonstro(struct inimigo *enemy);
 void iniciarCombate(struct jogador *player);
+void subirLevel(struct jogador *player);
 
 #endif //EMPTY_HOLLOW_COMBATE_H
