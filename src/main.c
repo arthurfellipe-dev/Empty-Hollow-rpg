54353457398ec4bf6c3905547abb5b7fa50c406a
Arthur Fellipe <vpneuaif@gmail.com>
2025-11-22 19:24:32 -0300

    Add game save functionality and adjust menu options

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include "player.h"
 #include "combate.h"
 #include "world.h"
-#include "itens.h"
+#include "storage.h"
 
 int main() {
     srand(time(NULL));
@@ -11,23 +11,38 @@ int main() {
     struct jogador player;
 
     titulo();
-    sleep(1);
+    sleep(2);
 
-    playerClasse(&player);
+    printf("\n\n1-carregar jogo\n");
+    printf("2-novo jogo\n");
+    scanf("%d", &escolhaPlay);
+    limpar_buffer();
+
+    if (escolhaPlay == 1)
+        carregar_progresso(&player);
+    else
+        playerClasse(&player);
+
+    sleep(2);
 
     do {
         printf("\n      Menu principal: \n\n");
+        printf("0-Sair\n");
         printf("1-Combate\n");
         printf("2-Loja\n");
         printf("3-Status\n");
         printf("4-Mudar dimenção\n");
         printf("5-Créditos\n");
-        printf("6-Sair\n");
+        printf("6-salvar jogo\n");
         scanf("%d", &escolhaPlay);
 
         limpar_buffer();
 
         switch(escolhaPlay) {
+
+        case 0:
+            printf("\nobrigado por jogar");
+            break;
         case 1:
             iniciarCombate(&player);
             break;
@@ -38,19 +53,18 @@ int main() {
             verStatus(&player);
             break;
         case 4:
-            printf("\nem deselvolvimento(não sei fazer, AINDA");
+            printf("\nem deselvolvimento(não sei fazer, AINDA)");
             break;
         case 5:
             verCreditos();
             break;
         case 6:
-            printf("\nobrigado por jogar");
-            break;
+            salvar_progresso(&player);
         default:
             printf("inválido");
             break;
         }
-    }while(escolhaPlay != 6);
+    }while(escolhaPlay != 0);
 
     return 0;
 }
